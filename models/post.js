4246a4089e09b5a6bb35fc144014b3ec28fb495b
Luiz Sócrate <luiz@socrate.com.br>
2013-09-30 03:11:22 -0300

    Add posted in field on Post using sprintf

--- models/post.js
+++ models/post.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var sprintf = require('sprintf-js').sprintf;
 var Schema = mongoose.Schema;
 
 var PostSchema = new Schema({
@@ -8,6 +9,9 @@ var PostSchema = new Schema({
   comments: [{ body: String, date: Date }],
   tags: [String],
   created: { type: Date, default: Date.now }
+});
+PostSchema.virtual('posted_in').get(function () {
+  return sprintf('%02d/%02d/%04d %02d:%02d', this.created.getDate(), this.created.getMonth(), this.created.getYear() + 1900, this.created.getHours(), this.created.getMinutes());
 });
 
 module.exports = mongoose.model('Post', PostSchema);
