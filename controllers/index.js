ed0bc288d149e6a7614f088e93067c49205b0f9a
Luiz Sócrate <luiz@socrate.com.br>
2013-10-01 01:14:48 -0300

    Index lists posts in reverse chronological order

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ var Post = require('../models/post');
 var User = require('../models/user');
 
 exports.index = function(req, res){
-  Post.find().populate('author', 'name', 'User').exec(function (err, posts) {
+  Post.find().populate('author', 'name', 'User').sort({lastTouched: -1}).exec(function (err, posts) {
     if (err) return;
 
     var isViewerLogged = !!req.user;
