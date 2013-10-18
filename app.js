fa6d1df8325f9b487c3a7acdacd6746b0c490440
Luiz Sócrate <luiz@socrate.com.br>
2013-10-18 12:17:56 -0300

    comment

--- app.js
+++ app.js
@@ -65,4 +65,6 @@ http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
+
+// EXPORT FOR TESTS
 exports = module.exports = app
